012ca63e4806f48259105182becc566e5196c636
StewLemin <mateidima@gmail.com>
2026-09-03 16:47:45 +0200

    Added Win/Loss Mechanic, have to change lose screen tho

---
+++ Assets/Scripts/TimerScript.cs
@@ -0,0 +1,65 @@
+using UnityEngine;
+using TMPro;
+
+public class TimerScript : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI timerText;
+    
+    [SerializeField] Canvas loseScreen;
+
+    [SerializeField] float initialTime = 20f;
+
+    private float currentTime;
+
+    private float minTime = 0;
+    
+    private bool active = true;
+
+    void Start()
+    {
+        currentTime = initialTime;
+        loseScreen.enabled = false;
+    }
+
+    public void StopTimer()
+    {
+        active = false;
+    }
+
+    public float getElapsedTime()
+    {
+        return initialTime - currentTime;
+    }
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!active)
+        {
+            return;
+        }
+
+        if (currentTime > 0)
+        {
+            currentTime -= Time.deltaTime;
+        }
+        else
+        {
+            currentTime = 0;
+            loseScreen.enabled = true;
+        }
+        
+        int minutes = Mathf.FloorToInt(currentTime / 60);
+        int seconds = Mathf.FloorToInt(currentTime % 60);
+        int milisec = Mathf.FloorToInt(currentTime * 100 % 100);
+        
+        string final = string.Format("{0}:{1:00}:{2:00}",minutes,seconds,milisec);
+        timerText.text = final;
+    }
+    
+        
+
+    
+}
